60f973444aad7b49722c98b170082eabd313450d
Naveen Pattanashetti <pattanashettinaveen.com>
2021-10-22 21:34:27 +0530

    Added:Welcome To Mood Analyzer Program

---
+++ src/main/java/com/bridgelabz/MoodAnalyzer/MoodAnalyzer.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.MoodAnalyzer;
+
+public class MoodAnalyzer {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to mood analyzer problem");
+
+	}
+
+}
