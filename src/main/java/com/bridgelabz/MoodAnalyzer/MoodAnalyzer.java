f0e31f8ab614a09bff3890294768a8345c99e573
Naveen Pattanashetti <pattanashettinaveen.com>
2021-10-22 21:44:02 +0530

    Added : Program for the Handle Exception error if user provide null

--- src/main/java/com/bridgelabz/MoodAnalyzer/MoodAnalyzer.java
+++ src/main/java/com/bridgelabz/MoodAnalyzer/MoodAnalyzer.java
@@ -1,14 +1,24 @@
 package com.bridgelabz.MoodAnalyzer;
 
 public class MoodAnalyzer {
+	private String message;
+
 	public MoodAnalyzer() {
 	}
 
-	public String MoodAnalyzer(String message) {
-		if (message.contains("Sad"))
-			return "SAD";
-		else
-			return "HAPPY";
+	public MoodAnalyzer(String message) {
+		this.message = message;
 	}
 
+	public String analyseMood() {
+		try {
+
+			if (this.message.contains("Sad"))
+				return "SAD";
+			else
+				return "HAPPY";
+		} catch (NullPointerException e) {
+			return "HAPPY";
+		}
+	}
 }
